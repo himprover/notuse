2a4ac5972642331481ff9aedfe0c1fb95053ab7f
himprover <hwang.improver@gmail.com>
2021-08-19 16:49:35 +0900

    gatsby_typescript setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,15 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+const IndexPage: React.FC = () => (
+  <Layout>
+    <Seo title="HOME" />
+    <h1>Hello TypeScript</h1>
+    <p>Welcome</p>
+    <Link to="/page-2/">Go to Page 2</Link>
+  </Layout>
+);
+/*
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
@@ -25,5 +34,6 @@ const IndexPage = () => (
     </p>
   </Layout>
 )
+*/
 
 export default IndexPage
